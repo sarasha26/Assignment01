20fdeacf80d4da410b60c95c1afcd20a84d781c2
Saranya Shankar <saranya.shankar@yahoo.com>
2024-10-23 12:07:14 -0700

    Assignment 01 changes

---
+++ js/script.js
@@ -0,0 +1,144 @@
+//STEP 1
+// let someMonth
+// function theMonth(){}
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+//STEP 2
+// let n = 20 //numeric 
+// let str = 'Welcome'  //string 
+// let bool = true //boolean
+// let nullval = null //null
+
+//STEP 3
+// let expr1 = (7*4) + 10
+// let expr2 = 3+(25/5);
+
+//STEP 4
+// let firstName,strLastName,address,strCity,state,zipCode,intYourAge,referralSource,boolMayWeContactYou;
+
+//STEP 5
+//method 1
+// let firstName;
+// let strCity
+// let zipCode
+// firstName = "Sara"
+// strCity = "San Diego"
+// zipCode = "92126";
+
+//method 2
+// let firsName = "Sara"
+// let strCity = "San Diego"
+// let zipCode = "92126"
+
+// method 3
+// let firsName = "Sara", strCity = "San Diego", zipcode = "92126"
+
+
+//STEP 6
+// let num = 10
+// let str = "Hello Welcome"
+// console.log(`The output of num + str is ${num+str}`)
+
+//STEP 7
+// let bool = true
+// let str = "Hello Welcome"
+// console.log(`The output of bool + string is ${bool+str}`)
+
+//STEP 8
+// let bool = true
+// let num = 9
+// console.log(`The output of bool + num is ${bool+num}`)
+
+//STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+//STEP 9
+// let nullval = null
+// console.log(nullval)
+// let emptyval
+// console.log(emptyval)
+
+//STEP 10
+// let str = "Hello"
+// let num = 10
+// let bool = true
+// let car = new Object()
+// let undef 
+
+// console.log(`type of str is ${typeof(str)}, type of num is ${typeof(num)},type of bool is ${typeof(bool)},type of car is ${typeof(car)},type of undef is ${typeof(undef)}`)
+
+
+//STEP 11
+// alert("Hello"+" Saranya"+ ", "+"welcome to the JavaScript class!")
+
+//STEP 12
+// let name = "Saranya"
+// alert("Hello"+" "+name+ ", "+"welcome to the JavaScript class!")
+
+//STEP 13
+// let greeting = "Hello "
+// let name = "Saranya"
+// let str1 = ", welcome to the "
+// let course = "JavaScript"
+// let str2 = " class!"
+// alert(greeting+name+str1+course+str2)
+
+//STEP 14
+// let greeting = "Hello "
+// let name = "Saranya."
+// let str1 = "Welcome to the "
+// let course = "JavaScript"
+// let str2 = " class!"
+// alert(greeting+name+"\n"+str1+course+str2)
+
+//STEP 15
+// let greeting = "Hello "
+// let name = prompt("Please enter your name:")
+// let str1 = "Welcome to the "
+// let course = "JavaScript"
+// let str2 = " class!"
+// alert(greeting+name+"."+"\n"+str1+course+str2)
+
+//STEP 16
+// let greeting = "Hello "
+// let name = prompt("Please enter your name:")
+// let str1 = "Welcome to the "
+// let course = prompt("Please enter the course you are taking:")
+// let str2 = " class!"
+// alert(greeting+name+"."+"\n"+str1+course+str2)
+
+//STEP 17
+// let num1 = 10
+// let num2 = 20
+// console.log(`The sum is ${num1+num2}`)
+
+//STEP 18
+// let x = 20
+// x += 20
+// console.log(`The result is ${x}`)
+
+//STEP 19
+// let x = 20
+// x *= 5
+// console.log(`The result is ${x}`)
+
+//STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(`The result is ${x}`)
+
+//STEP 21
+// let x = 100/5
+// let y = "Hello"
+// console.log((x > 0 && y != null))
+
+//STEP 22
+// let x = 100/20
+// let y = null
+// console.log((x > 0 && y != null))
+
+
+
